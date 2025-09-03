8a5a1a4075ee5a6a0358d3bc1425392a990d1974
Signe Kjær Brandt Hau <signehau@gmail.com>
2025-09-03 11:54:20 +0200

    nyt navn på index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 require '../.env';
 
-header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
